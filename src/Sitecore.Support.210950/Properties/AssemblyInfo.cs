de7a581aaaec30480dfa65673f93e35c38a565ed
AndreyFilchenkov <AndreyFilchenkov@users.noreply.github.com>
2018-03-07 18:01:43 +0200

    Init commit

---
+++ src/Sitecore.Support.210950/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.210950")]
+[assembly: AssemblyProduct("Sitecore.Support.210950")]
+[assembly: ComVisible(false)]
